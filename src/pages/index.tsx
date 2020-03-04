c72b67e8fb703ed4b6da7004f36a9db75398066a
Alex Labonne <>
2020-03-04 22:33:50 +0100

    chore: add ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Link to="questions">Go to questions</Link>
+  </Layout>
+)
+
+export default IndexPage
--- src/pages/questions.tsx
+++ src/pages/questions.tsx
@@ -6,9 +6,8 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <SEO title="Questions" />
+    <h1>Choose a question</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
